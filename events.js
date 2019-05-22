ddba98e7f317839d2ac96ab119934217b6686f8a
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2019-05-22 21:16:02 +0300

    server

---
+++ events.js
@@ -0,0 +1,12 @@
+module.exports = {
+  CONNECTION       : 'connection',
+  JOIN_USER        : 'join user',
+  USER_JOINED      : 'user joined',
+  NEW_MESSAGE      : 'new message',
+  MESSAGE_RECEIVED : 'message received',
+  USER_LEFT        : 'user left',
+  DISCONNECT       : 'disconnect',
+  RECONNECT        : 'reconnect',
+  START_TYPING     : 'start typing',
+  STOP_TYPING      : 'stop typing',
+};
---
+++ index.js
@@ -0,0 +1,76 @@
+const Server = require('socket.io'),
+      Chat   = require('./factory/Chat'),
+      e      = require('./events');
+
+const io = new Server(5000);
+
+// Event listeners
+io.on(e.CONNECTION, (socket) => {
+  let userJoined = false;
+
+  socket.on(e.JOIN_USER, (userName, roomId) => {
+    console.log(e.JOIN_USER);
+    const { user, room } = chat.join(userName, roomId);
+    socket.room = room;
+
+    if (!userJoined) {
+      socket.user = user;
+      socket.join(user.room);
+      userJoined = true;
+    }
+
+    io.to(user.room).emit(e.USER_JOINED, {
+      user: {
+        id: user.id,
+        name: user.name,
+        room: user.room
+      },
+      users: room.users
+    })
+  });
+
+  socket.on(e.NEW_MESSAGE, (message) => {
+    console.log(e.NEW_MESSAGE);
+    io.to(message.room).emit(e.MESSAGE_RECEIVED, {
+      user: {
+        id: message.user.id,
+        name: message.user.name
+      },
+      message: message.body,
+      timestamp: new Date().getTime()
+    });
+  });
+
+  socket.on(e.START_TYPING, () => {
+    const { user } = socket;
+    if (!user) return;
+    io.to(user.room).emit(e.START_TYPING, {
+        name: user.name,
+        id: user.id
+    });
+  });
+
+  socket.on(e.STOP_TYPING, () => {
+    const { user } = socket;
+    if (!user) return;
+    io.to(user.room).emit(e.STOP_TYPING, user.id);
+  });
+
+  // socket.on(e.RECONNECT, () => userJoined = false);
+
+  socket.on(e.DISCONNECT, () => {
+    console.log(e.DISCONNECT);
+    const { user } = socket;
+    if (!user) return;
+
+    chat.leave(user.id, user.room);
+    socket.to(user.room).emit(e.USER_LEFT, {
+      user: {
+        name: user.name
+      },
+      users: socket.room.users
+    });
+  });
+});
+
+const chat = new Chat();
